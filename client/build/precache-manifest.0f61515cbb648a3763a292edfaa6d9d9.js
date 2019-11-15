786b58337bac7c154d90a5af0c9013e5ddc91485
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-11-15 00:12:00 -0500

    did yarn build

---
+++ client/build/precache-manifest.0f61515cbb648a3763a292edfaa6d9d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6d721ce5e4677ad066fe",
+    "url": "/static/js/main.ea8d81ba.chunk.js"
+  },
+  {
+    "revision": "fd81d7c7cc67e24d2969",
+    "url": "/static/js/2.48814cdb.chunk.js"
+  },
+  {
+    "revision": "6d721ce5e4677ad066fe",
+    "url": "/static/css/main.fe44a750.chunk.css"
+  },
+  {
+    "revision": "5fe905102eafa2cc86aacc1718f9d0b5",
+    "url": "/index.html"
+  }
+];
